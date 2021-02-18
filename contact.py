d9c42df620e2ace41b3b8bb38da158b446a73637
danielsind <danielsindet@gmail.com>
2021-02-18 14:59:04 +0300

    first commit

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact :
+    def _init_(self,first_name,last_name,email,phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        
